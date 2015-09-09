c128ccfd4b708acdb2e222a6a80d0ee36c8937e7
David Thole <dthole@gmail.com>
2015-09-09 09:43:18 -0500

    Updates to the gitignore picking up a few more files

--- Learning-FSharp/ch02.fs
+++ Learning-FSharp/ch02.fs
@@ -1,3 +1,17 @@
+// Caching
+open System.Collections.Generic
+let rec fibonacci_Generic n =
+  let cache = Dictionary<_, _>()
+  let rec fibonacciX = function
+    | n when n = 0I -> 0I
+    | n when n = 1I -> 1I
+    | n -> fibonacciX (n - 1I) + fibonacciX (n - 2I)
+  if cache.ContainsKey(n) then cache.[n]
+  else
+    let result = fibonacciX n
+    cache.[n] <- result
+    result
+
 let rec TowerOfHanoi f x t n =
   if n > 0 then
     TowerOfHanoi f t x (n - 1)
@@ -7,9 +21,22 @@ let rec TowerOfHanoi f x t n =
 ;;
 
 // Alternate Option
-
+// Research Paper: http://cs.cornell.edu/~kozen/papers/theory.pdf
 let rec TowerOfHanoiRec n s f =
   match n with
     | 0 -> [ ]
     | _ -> let t = (6 - s - f)
            (TowerOfHanoiRec (n-1) s t) @ [s, f] @ (TowerOfHanoiRec(n-1) t f)
+
+// The @ symbol concatenates 2 lists (See ch03 of Expert F# 3.0)
+           
+
+;;
+// Lazy Searching
+           
+
+// Sorting
+let listPrint = function
+  | [] -> []
+  | n::ns -> printfn "n => %i" n
+             ns
